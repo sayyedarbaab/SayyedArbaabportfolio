dd9d405da74ba892f0d2b8573a45b1da93a2fba3
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-01-21 21:31:28 +0530

    add commit

--- my-portfolio/src/HeroComponent/Hero.jsx
+++ my-portfolio/src/HeroComponent/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter, Link } from "react-router-dom"
+// import { BrowserRouter, Link } from "react-router-dom"
 import styled from "styled-components"
 
 
@@ -7,19 +7,26 @@ function Hero(){
     return( 
         <MainContainer>
         <div className="image-container">
-            <img src="imgArbab.jpg" height="350vh"/>
+            <img src="imgArbab.jpg" height="350vh" alt="admin_img"/>
         </div>
         <div className="title-container">
         <h2>Hello,I m</h2>
         <h1>Sayyed Arbaab</h1>
         <h2>Frontend Developer</h2>
-        <button>Download Cv</button>
+        <div className="btn-container">
+        <button>Download CV</button>
         <button>Contact Info</button>
+        </div>
+        <div className="logo-container">
+        <img src="github-sign.png" alt="gitLogo"/>
+        <img src="linkedin.png" alt="linkedlnLogo" />
+        </div>
         </div>
         </MainContainer>
     )
 }
 export default Hero
+
 
 const MainContainer=styled.div`
 background:black;
@@ -42,9 +49,10 @@ display:flex;
 .title-container{
     background:gold;
     width:30%;
-    height:40vh;
+    height:50vh;
     margin: auto;
     margin-left:40px;
+    line-height:28px;
     h1{
         text-align:center;
         font-family: 'Heebo', sans-serif;
@@ -54,9 +62,32 @@ display:flex;
         font-family: 'Heebo', sans-serif;
         color:gray;
     }
-    button{
-        border-radius:4px;
-        height:4vh;
+    .btn-container{
+        height:5vh;
+        margin:auto;
+        display:flex;
+        gap:10px;
+        justify-content:center;
+
+        button{
+            font-family: 'Heebo', sans-serif;
+            border-radius: 20px;
+            cursor:pointer;
+            color:black;
+            height:6vh;
+            width:28%;      
+        }
+    }
+    .logo-container{
+        height:7vh;
+        margin:auto;
+        display:flex;
+        gap:10px;
+        justify-content:center;
+
+        img{
+            margin-top:14px;
+        }
     }
 }
 `
